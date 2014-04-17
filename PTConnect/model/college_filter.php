faf6cae480b5d7fa8e6bed377c39cbf206903316
dannygmoe <dan5006@comcast.net>
2014-04-17 12:02:49 -0400

    Filter Begin
    start of the college filter page

---
+++ PTConnect/model/college_filter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Danny
+ * Date: 4/17/14
+ * Time: 11:55 AM
+**/
+
+
+?>
+<html>
+<style>
+    body{
+        display: inline;
+    }
+</style>
+<title>
+    College Filter
+</title>
+<body>
+<div>
+
+</div>
+<div>
+
+</div>
+</body>
+</html>
