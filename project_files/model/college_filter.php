01083bcf22ea7ce067b3fe617e8e45efda68b6b9
dannygmoe <dan5006@comcast.net>
2014-04-18 14:14:22 -0400

    filter page changes

--- project_files/model/college_filter.php
+++ project_files/model/college_filter.php
@@ -4,39 +4,102 @@
  * User: Danny
  * Date: 4/17/14
  * Time: 11:55 AM
-**/
-
+ **/
 
 ?>
 <html>
+
+<!-- insert header here -->
+
 <style>
     body{
-        display: inline;
+        display: inline-block;
     }
     div:first-child{
-        border: 1px solid red;
         /*width and height customized to a 1px border*/
         width: 30%;
         height: 99.72%;
-        float: left;
     }
-    div:nth-of-type(2){
-        border: 1px solid red;
+
+    div:nth-child(2){
+        height: 100%;
+        width: 1px;
+        border: 1px solid black;
+    }
+
+    div:nth-of-type(3){
         /*width and height customized to a 1px border*/
-        width: 69.72%;
+        width: 30%;
         height: 99.72%;
-        float: right;
+        margin-left: 30%;
+    }
+
+    p{
+        margin-left: 10%;
+        margin-bottom: -1%;
+        margin-top: 3%;
+    }
+
+    select{
+        width: 80%;
+        margin-left: 10%;
     }
 </style>
 <title>
     College Filter
 </title>
-<body>
-<div>
-    woop
+<body style="display: inline">
+<div class="fltrs" style="margin: 0">
+    <h1 style="margin-left: 10%">Filters</h1>
+    <div style="height: 0; border: 1px solid black; margin-left: 10%; margin-right: 10%; text-align: center; margin-top: -5%"></div>
+    <p>PT Grad Year</p>
+    <select>
+        <!-- options -->
+    </select>
+
+    <p>College</p>
+    <select>
+        <!-- options -->
+    </select>
+
+    <p>Major</p>
+    <select>
+        <!-- options -->
+    </select>
+
+    <p>Experience Type</p>
+    <select>
+        <!-- options -->
+    </select>
+
+    <p>Experience Title</p>
+    <select>
+        <!-- options -->
+    </select>
 </div>
 <div>
-    woop
+
+</div>
+<div>
+    <!-- php for statement making rows of 4 profiles -->
+    <div style="display: inline">
+        <div>
+            bing
+        </div>
+        <div>
+            bing
+        </div>
+        <div>
+            bing
+        </div>
+        <div>
+            bing
+        </div>
+    </div>
+    <!-- end statement -->
 </div>
 </body>
+
+<!-- insert footer here -->
+
 </html>
