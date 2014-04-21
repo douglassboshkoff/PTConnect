bf7a048ca07b434b7fc1edfb0c2ffd72c26bc926
afjohnston <braveguy96@gmail.com>
2014-04-21 15:05:49 -0400

    Profile Page Started

---
+++ project_files/view/User_Profilepage.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Adam
+ * Date: 4/21/14
+ * Time: 2:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+<html>
+    <head>
+        <title> Profile Page</title>
+        <style>
+            #main{
+                border: solid black 1px;
+                width: 35em;
+                margin: auto;
+                padding-left: 1em;
+                padding-right: 1em;
+                padding-top: 1em;
+                padding-bottom: 1em;
+            }
+            .sub{
+                border: solid black 1px;
+                height: 10em;
+                width: 35em;
+                margin: auto;
+                margin-bottom: 1em;
+            }
+            #div1{
+                border: none;
+            }
+
+        </style>
+    </head>
+    <body>
+    <div id = "main">
+        <div id = "div1" class = "sub">
+
+        </div>
+        <div class = "sub">
+
+        </div>
+        <div class = "sub">
+
+        </div>
+        <div class = "sub">
+
+        </div>
+    </div>
+    </body>
+</html>
