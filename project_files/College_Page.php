3c512ed9eb0a38e1bc29eada40e681455689ec33
afjohnston <braveguy96@gmail.com>
2014-04-21 14:50:25 -0400

    College_Page Functionality and Outline v1 Complete
    College Page can now switch between questions and has a scroll bar
    containing the comments. Need SQL methods/database for solidifying it.

--- project_files/College_Page.php
+++ project_files/College_Page.php
@@ -1,17 +1,17 @@
 <?php
 
 $comment_arr = array();
+
+/** Will correspond with the questions held in $arr (if page is on question 2, these will be question 2 comments */
 $comment_arr[0] = "Comment 1";
 $comment_arr[1] = "Comment 2";
 $comment_arr[2] = "Comment 3";
 $comment_arr[3] = "Comment 4";
 $comment_arr[4] = "Comment 5";
-//$comment_arr[5] = "Comment 6";
-//$comment_arr[6] = "Comment 7";
-//$comment_arr[7] = "Comment 8";
-//$comment_arr[8] = "Comment 9";
+$comment_arr[5] = "Comment 6";
 
 
+/** $arr holds the values of the questions, $pageNum/$_GET[pageNum] will determine which question is used */
 $arr = array();
 $arr[0] = "test";
 $arr[1] = "test1";
@@ -19,6 +19,7 @@ $arr[2] = "test2";
 $arr[3] = "test3";
 $arr[4] = "test4";
 
+/** Manages what question page the page is on (cycles through them using the q_next and q_prev divs */
 if(isset($_GET['pageNum'])){
     $pageNum = $_GET['pageNum'];
 }else{
@@ -42,33 +43,39 @@ if($_GET['next'] == 1){
 <head>
     <title> WEBPAGE </title>
     <style>
-
+        /** Generic Div **/
         div{
             height: 5em;
             width: 5em;
             border: 1px black solid;
             display: inline-block;
         }
+        /** Div within the main div containing the comments div and questions div**/
         #post_bar{
 
             width: 30em;
-            //height: 18em;
-            height: <?php echo (count($comment_arr) * 6); ?>em;
-            //padding-bottom: 1em;
+            height: 18em;
+            //height: <?php echo (count($comment_arr) * 6); ?>em;
+            padding-bottom: 1em;
+            overflow: auto;
         }
+        /** Div used to go to previous question (Contains left arrow image)**/
         #prev_q{
             float:left;
             margin-left: 2.5em;
 
         }
+        /** Holds the question **/
         #question_display{
             width: 19em;
             margin-left: .3em;
         }
+        /** Div used to go to next question (Contains right arrow image) **/
         #next_q{
             margin-right: 2.5em;
             float:right;
         }
+        /** Div containing the top half of the page (User Bio, Image) (not the name of college)**/
         #main1{
             margin: auto;
             padding-top: 1em;
@@ -76,23 +83,27 @@ if($_GET['next'] == 1){
             height: 15em;
             text-align: center;
         }
+        /** Contains bottom half of page (comments area/questions)**/
         #main2{
             margin: auto;
             padding-top: 1em;
             width: 35em;
-            //height: 25em;
+            height: 25em;
             text-align: center;
-            height: <?php echo (count($comment_arr) * 7); ?>em;
+            //height: <?php echo (count($comment_arr) * 7); ?>em;
+            //overflow: auto;
 
         }
         body{
             text-align: center;
         }
+        /** Arrow Images **/
         .arrows{
             margin-left: .2em;
             margin-top: .6em;
         }
-        #user_pic{
+        /** Pic of the college**/
+        #coll_pic{
             float: left;
             height: 80%;
             width: 35%;
@@ -113,13 +124,15 @@ if($_GET['next'] == 1){
             text-align: center;
         }
         /** Code From Danny **/
+        /** Direct div containing comments **/
         #clip {
             margin-top: 1em;
             height: 100%;
             width: 90%;
             border: none;
+            //overflow: auto;
         }
-
+        /** Listing the comments code**/
         ul{
             list-style-type: none;
             padding: 0;
@@ -149,7 +162,7 @@ if($_GET['next'] == 1){
 </div>
 <br/>
 <div id = "main1">
-    <div id = "user_pic">
+    <div id = "coll_pic">
         <img src = "#" />
     </div>
     <div id = "college_bio">
