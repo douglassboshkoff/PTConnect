08b6e287fed47eda3fec577121b209b36d62ff81
rschuckit <rubenschuckit@gmail.com>
2014-04-24 14:09:43 -0400

    Test submit

---
+++ project_files/model/university_db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rubenschuckit
+ * Date: 4/24/14
+ * Time: 1:44 PM
+ */
+
+function getCollege($userID) {
+    $query = "SELECT university_id FROM questions WHERE account_id == '$userID'";
+
+
+}
