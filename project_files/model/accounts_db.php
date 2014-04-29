0620a29fd746533781ffb92c8f82aded927984a4
mmuhober <mmuhoberac@parktudor.org>
2014-04-29 09:34:24 -0400

    Updated 2 methods 1 should work, 1 needs some additional phpcode

--- project_files/model/accounts_db.php
+++ project_files/model/accounts_db.php
@@ -1,11 +1,19 @@
 <?php
 
-//note, this does not work atm
-//test 2
-
     function addUser($fname, $lname, $email, $gradYear) {
+        global $db;
         $query = 'INSERT INTO accounts( first_name, last_name, email, pt_grad_year) 
          VALUES ('$fname', '$lname', '$email', '$gradYear')';
+         $db->exec($query);
+         //this should work now assuming all of my syntax is correct. I cannot get it to work on phpstorm at this time
+    }
+    
+    function getUser($email) {
+        global $db;
+        $query = "SELECT * FROM accounts WHERE email == '$email'"
+        $user = $db->query($query);
+        //this needs php code to return the acutal user, cannot write this at this time.
+        //would give you cookie if you wrote this for me.
     }
 
 ?>
