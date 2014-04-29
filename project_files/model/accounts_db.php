e0f82ccead51b319c40fb5a5d7f5a8a7e6fc18c3
mmuhober <mmuhoberac@parktudor.org>
2014-04-29 09:16:41 -0400

    Works for me when I test in php admin

--- project_files/model/accounts_db.php
+++ project_files/model/accounts_db.php
@@ -3,9 +3,9 @@
 //note, this does not work atm
 //test 2
 
-function addUser($fname, $lname, $email, $gradYear) {
-    $query = 'INSERT INTO accounts( first_name, last_name, email, pt_grad_year) 
-      VALUES ('$fname', '$lname', '$email', '$gradYear')';
-  }
+    function addUser($fname, $lname, $email, $gradYear) {
+        $query = 'INSERT INTO accounts( first_name, last_name, email, pt_grad_year) 
+         VALUES ('$fname', '$lname', '$email', '$gradYear')';
+    }
 
 ?>
