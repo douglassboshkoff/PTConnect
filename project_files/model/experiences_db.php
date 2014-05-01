708bdc640fb16fc81382715a1a6f220a1583dcdb
rschuckit <rubenschuckit@gmail.com>
2014-05-01 13:08:09 -0400

    gets college name by user (account) ID

--- project_files/model/experiences_db.php
+++ project_files/model/experiences_db.php
@@ -6,3 +6,26 @@ function get_experiences($id) {
     $result = $db->query($query);
     return $result->fetch();
 }
+
+function get_content($id) {
+    global $db;
+    $query = "SELECT content FROM experiences WHERE accounts_id_ac = '$id'";
+    $result = $db->query($query);
+    return $result->fetch();
+}
+
+function get_all_titles() {
+    global $db;
+    $query = "SELECT title FROM experiences";
+    $result = $db->query($query);
+    $result = $result->fetch();
+    $toReturn = array();
+
+    foreach($result as $r) {
+        $toReturn[] = $r;
+    }
+
+    return $toReturn;
+
+    //returns an array
+}
