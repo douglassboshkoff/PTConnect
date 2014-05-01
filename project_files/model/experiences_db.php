6dc031f5ae25733add8e2e9f03b66572fdf16e5f
mmuhober <mmuhoberac@parktudor.org>
2014-05-01 13:06:49 -0400

    added get_types method

--- project_files/model/experiences_db.php
+++ project_files/model/experiences_db.php
@@ -6,3 +6,14 @@
  * Time: 1:32 PM
  * To change this template use File | Settings | File Templates.
  */
+ 
+  function get_types() {
+    global $db;
+    $query = 'SELECT DISTINCT type
+    FROM experiences
+    ORDER BY type';
+    $result = $db->query($query);
+    $types = $result->fetch();
+    return $types;
+  }
+?>
