0f8270166120134dca2fd678caa010a9d2871fe8
afjohnston <braveguy96@gmail.com>
2014-05-05 12:37:26 -0400

    sadf

--- project_files/model/questions_db.php
+++ project_files/model/questions_db.php
@@ -1,5 +1,5 @@
 <?php
-
+//Shouldn't this be getComments()/getAnswers()?
 function getQuestion($userID, $collegeID, $questionID) {
     $query = "SELECT * FROM questions
 	      WHERE accounts_id_ac == '$userID' AND question_id == '$questionID' AND university_id_un == '$collegeID'";
