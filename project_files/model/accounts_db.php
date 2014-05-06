5d64db168aa0a2fadb9a1417afdcc2de98740605
mmuhober <mmuhoberac@parktudor.org>
2014-05-06 15:34:31 -0400

    aaa

--- project_files/model/accounts_db.php
+++ project_files/model/accounts_db.php
@@ -1,6 +1,6 @@
 <?php
 
-    function addUser($fname, $lname, $email, $gradYear) {
+    function add_user($fname, $lname, $email, $gradYear) {
         global $db;
         $query = "INSERT INTO accounts( first_name, last_name, email, pt_grad_year) 
          VALUES ('$fname', '$lname', '$email', '$gradYear')";
@@ -8,16 +8,21 @@
          //this should work now assuming all of my syntax is correct. I cannot get it to work on phpstorm at this time
     }
     
-    function getUser($email) {
+    function get_user($email) {
         global $db;
-        $query = "SELECT * FROM accounts WHERE email == '$email'"
+        $query = "SELECT * FROM accounts WHERE email == '$email'";
         $user = $db->query($query);
-        //this needs php code to return the acutal user, cannot write this at this time.
-        //would give you cookie if you wrote this for me.
-        //needs an object class
+        return $user;
     }
     
-    function validatePassword($email, $password) {
+    function get_user_by_id($id) {
+        global $db;
+        $query = "SELECT * FROM accounts WHERE email == '$id'";
+        $user = $db->query($query);
+        return $user;
+    }
+    
+    function validate_password($email, $password) {
         global $db;
         $query = "SELECT * FROM accounts WHERE email == '$email'";
         $result = $db->query($query);
@@ -30,6 +35,65 @@
             return false;
         }
         //hope this workds
+    }
+
+    function get_bio($id) {
+        global $db;
+        $query = "SELECT bio FROM accounts WHERE id = '$id'";
+        $result = $db->query($query);
+        return $result->fetch();
+    }
+    
+   function get_grad_years() {
+        global $db;
+        $query = "SELECT DISTINCT pt_grad_year
+        FROM accounts
+        ORDER BY pt_grad_year";
+        $result = $db->query($query);
+        $years = $result->fetch();
+        return $years;
+
+        //the sql command works, however the phpcode might not.
+    }
+
+    function get_name($id) {
+        global $db;
+        $query = "SELECT first_name
+        FROM accounts
+        WHERE id = '$id'";
+        
+    }
+    function get_users(){
+    global $db;
+    $query="SELECT * FROM accounts";
+    $result=$db->query($query);
+    return $result;
+    }
+
+
+    function get_college_by_user($id) {
+    global $db;
+    $query = "SELECT name FROM universities
+    INNER JOIN questions
+    ON questions.university_id = universities.id
+    WHERE questions.accounts_id = '$id'";
+    $result = $db->query($query);
+    return $result->fetch();
+    }
+    
+    function get_names_by_filter($arryId) {
+        global $db;
+        $results = array();
+        $i = 0;
+        foreach($arryId as $id) {
+            $query = 'SELECT CONCAT(first_name, " ", last_name) as Name FROM accounts WHERE id = "$id"';
+            $name = $db->query($query);
+            $results[$i] = $name;
+            $i++;
+        }
+        return $results;
+        //I cannot check this function at this time, my phpstorm still does not work, this functions takes in an array of
+        // ids and returns and array of names. sorry if my syntax is wrong.
     }
 
 ?>
