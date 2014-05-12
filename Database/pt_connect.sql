d0b4a26456893e1785e0c51071a86ccad30ef175
rschuckit <rubenschuckit@gmail.com>
2014-05-12 13:42:28 -0400

    gets college name by user (account) ID

--- Database/pt_connect.sql
+++ Database/pt_connect.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 12, 2014 at 07:30 PM
+-- Generation Time: May 12, 2014 at 07:42 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -55,8 +55,8 @@ CREATE TABLE IF NOT EXISTS `concentration` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `major_minor` int(11) NOT NULL,
-  `universities_id_un` int(11) NOT NULL,
-  `accounts_id_ac` int(11) NOT NULL,
+  `universities_id` int(11) NOT NULL,
+  `accounts_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `concentration` (
 -- Dumping data for table `concentration`
 --
 
-INSERT INTO `concentration` (`id`, `name`, `major_minor`, `universities_id_un`, `accounts_id_ac`) VALUES
+INSERT INTO `concentration` (`id`, `name`, `major_minor`, `universities_id`, `accounts_id`) VALUES
 (1, 'Computer Science', 1, 1, 1);
 
 -- --------------------------------------------------------
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `experiences` (
   `type` varchar(45) NOT NULL,
   `title` varchar(45) NOT NULL,
   `content` varchar(200) NOT NULL,
-  `accounts_id_ac` int(11) NOT NULL,
+  `accounts_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
