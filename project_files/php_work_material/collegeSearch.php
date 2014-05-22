e1385f4554c0be30f8bb1e9190bf86e4f7bd5993
dannygmoe <dan5006@comcast.net>
2014-05-22 14:14:41 -0400

    merging php and html

--- project_files/php_work_material/collegeSearch.php
+++ project_files/php_work_material/collegeSearch.php
@@ -1,11 +1,43 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Danny
- * Date: 5/22/14
- * Time: 1:33 PM
- * To change this template use File | Settings | File Templates.
- */
+
+include "../model/university_db.php";
+include "../model/database.php";
+
+//test array
+$arr = array("goin?", "ggg", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "hello","world","how's","it","goin?", "ggg", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "geree", "aerffds", "wefds", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer","goin?", "ggg", "dwwer", "dwwer", "dwwer", "dwwer", "dwwer");
+
+// temporary value
+$collegeID = "Tom Ato";
+$class = 2015;
+
+if(isset($_POST['name'])){
+    $nameIn = $_POST['name'];
+}
+else{
+    $nameIn = "";
+}
+if(isset($_POST['location'])){
+    $locationIn = $_POST['location'];
+}
+else{
+    $locationIn = "";
+}
+if(isset($_POST['type'])){
+    $typeIn = $_POST['type'];
+}
+else{
+    $typeIn = "";
+}
+
+
+//filters
+$name_array = get_colleges();
+$location_array = get_college_locations();
+$type_array = array("hello","world","how's","it","goin?");
+
+//
+$secondary_info_array = "";
+$secondary_name_array = "";
 ?>
 <html>
 <head>
@@ -16,68 +48,66 @@
     <div id='wrapper2'>
         <div id="filters">
             <h1>Filters</h1>
+            <label>Name</label></br>
+            <select name="name" id="dropdown">
+                <?php if($nameIn != "") : ?>
+                    <option value="<?php echo $nameIn?>"><?php echo $nameIn ?></option>
+                <?php endif ; ?>
+                <?php foreach ($name_array as $name) : ?>
+                    <?php if($name['name'] != $nameIn) : ?>
+                        <option value="<?php echo $name['name'] ?>"><?php echo $name['name'] ?></option>
+                    <?php endif ; ?>
+                <?php endforeach ; ?>
+            </select>
+
             <label>Location</label></br>
-            <select id="dropdown">
-                <option>Location</option>
-                <option>2017</option>
-                <option>2016</option>
-                <option>2015</option>
-                <option>2014</option>
-                <option>2013</option>
-                <option>2012</option>
-                <option>2011</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2009</option>
-                <option>2008</option>
-                <option>2007</option>
-                <option>2006</option>
-                <option>2005</option>
-                <option>2004</option>
+            <select name="name" id="dropdown">
+                <?php if($locationIn != "") : ?>
+                    <option value="<?php echo $locationIn?>"><?php echo $locationIn ?></option>
+                <?php endif ; ?>
+                <?php foreach ($location_array as $location) : ?>
+                    <?php if($location['location'] != $locationIn) : ?>
+                        <option value="<?php echo $location['location'] ?>"><?php echo $location['location'] ?></option>
+                    <?php endif ; ?>
+                <?php endforeach ; ?>
             </select></br>
-            <label>Number Graduated</label></br>
-            <select id="dropdown">
-                <option>Number Graduated</option>
-                <option>2017</option>
-                <option>2016</option>
-                <option>2015</option>
-                <option>2014</option>
-                <option>2013</option>
-                <option>2012</option>
-                <option>2011</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2009</option>
-                <option>2008</option>
-                <option>2007</option>
-                <option>2006</option>
-                <option>2005</option>
-                <option>2004</option>
-            </select></br>
-            <label>Number Attending</label></br>
-            <select id="dropdown">
-                <option>Number Attending</option>
-                <option>2017</option>
-                <option>2016</option>
-                <option>2015</option>
-                <option>2014</option>
-                <option>2013</option>
-                <option>2012</option>
-                <option>2011</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2010</option>
-                <option>2009</option>
-                <option>2008</option>
-                <option>2007</option>
-                <option>2006</option>
-                <option>2005</option>
-                <option>2004</option>
+
+            <label>Type</label></br>
+            <select name="type" id="dropdown">
+                <option value=""></option>
+                <!--
+                <?php if($typeIn != "") : ?>
+                    <option value="<?php echo $typeIn?>"><?php echo $typeIn ?></option>
+                <?php endif ; ?>
+                <?php foreach ($type_array as $type) : ?>
+                    <?php if($type['type'] != $typeIn) : ?>
+                        <option value="<?php echo $type['type'] ?>"><?php echo $type['type'] ?></option>
+                    <?php endif ; ?>
+                <?php endforeach ; ?>
+                -->
             </select></br>
         </div>
         <div id="content">
+            <?php foreach($arr as $value) : ?>
+                <!-- add place for action -->
+                <form action="College_Page.php" method="post">
+                    <input type="hidden" name="college_ID" value="<?php echo $collegeID ?>">
+                    <div id="college">
+                        <input class="propic" type="image" src="../resources/College.jpg" alt="Submit">
+
+                        <div class="bottom">
+                            <div class="info">
+                                <div class="name">
+                                    <?php echo $value?>
+                                </div>
+                                <div class="class_number">
+                                    <?php echo $class?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            <?php endforeach; ?>
             <div id="college">
                 <a href="../html_originals/college.html"><img src="purdue.png"/></a>
                 <a href="../html_originals/college.html"><h2>Purdue University</h2></a>
