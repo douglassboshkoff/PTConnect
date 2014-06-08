bd700614d464d236321da4485480064e5da0f283
afjohnston <braveguy96@gmail.com>
2014-06-08 14:27:12 -0400

    Sync

--- project_files/php_work_material/profile.php
+++ project_files/php_work_material/profile.php
@@ -2,6 +2,7 @@
 include("../model/database.php");
 include("../model/university_db.php");
 include("../model/accounts_db.php");
+include("../model/experiences_db.php");
 
 $cur_pic = "../test_material/bullets_picture/Bullet_Arrow_R.png";
 //gets userID from alum filter
@@ -11,12 +12,24 @@ if (isset($_POST["userID"])) {
 }
 $g_colleges = get_college($userID);
 
+$arr_exp = array();
+
+$arr_exp[0] = "test";
+$arr_exp[1] = "test1";
+$arr_exp[2] = "test2";
+$arr_exp[3] = "test3";
+$arr_exp[4] = "test4";
+
+$arr_exp = get_experiences($userID)->fetchAll() ;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    <script type="text/javascript" src="../js/jQuery.js"></script>
+    <script type="text/javascript" src="../js/bulletslide.js"></script>
 </head>
+
 <body>
 <div id="profile">
     <div id="profilePicture">
@@ -45,9 +58,13 @@ $g_colleges = get_college($userID);
     <div id="experiences">
         <h1>Experiences</h1>
         <ul>
-            <li>RIS (Summer Camp) <a>>></a></li>
-            <li>Google, Inc. (Internship) <a>>></a></li>
-            <li>Miami of Ohio (CS Competition) <a>>></a></li>
+            <?php for($i = 0; $i < count($arr_exp); $i++):?>
+                <li>
+                    <div class = "expdiv"> <?php echo $arr_exp[$i]['title']; ?>  </div> <span class = "expander"> <img class = "exp_img" src =  "../test_material/bullets_picture/Bullet_Arrow_R.png";
+                </span>
+                    <div class="expand" style="display: none;"><?php echo $arr_exp[$i]['content']; ?></div>
+                </li>
+            <?php endfor; ?>
         </ul>
     </div>
 </div>
