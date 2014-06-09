0b747de81e72d36c52eb8fd3adc6851ccec33173
afjohnston <braveguy96@gmail.com>
2014-06-09 14:54:36 -0400

    asdf

--- project_files/php_work_material/index.php
+++ project_files/php_work_material/index.php
@@ -1,11 +1,107 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Danny
- * Date: 5/22/14
- * Time: 1:34 PM
- * To change this template use File | Settings | File Templates.
- */
+require('../model/database.php');
+require_once('../model/accounts_db.php');
+require_once('../model/concentration_db.php');
+require_once('../model/experiences_db.php');
+require_once('../model/queries_db.php');
+require_once('../model/questions_db.php');
+require_once('../model/university_db.php');
+
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'under_construction';
+}
+
+if ($action == 'under_construction')
+{
+    include('index.php');
+}
+else if($action == 'mycollege')
+{
+    /**
+     * something like...
+     * access the user that we're talking about...
+     * access all of their colleges...
+     * put those in an array?
+     * loop through array
+     * let those colleges populate the table...
+     */
+    $colleges = get_colleges();
+    include('header.php');
+    include('../view/mycollege.php');
+}
+else if($action == 'delete_college_from_user')
+{
+    /**
+     * we have the user...
+     * take the college name...
+     * delete it from the user himself...
+     */
+}
+else if($action == 'login')
+{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    if(validate_password($email, $password) == true){
+        include('header.php');
+        include('main.php');
+    }
+    else {
+        include('index.php');
+    }
+}
+else if($action == 'register')
+{
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $verifypassword = $_POST['verifypassword'];
+    $pt_grad_year = $_POST['pt_grad_year'];
+
+    add_user($first_name, $last_name, $email, $pt_grad_year, $password);
+
+    if($password == $verifypassword)
+    {
+        include('header.php');
+        include('main.php');
+    }
+    else
+    {
+        echo "This should create error text in the registration box.";
+    }
+}
+else if($action == 'continue_as_guest')
+{
+    include('main.php');
+}
+else if($action=='college_filter')
+{
+    include('../view/college_filter.php');
+}
+else if($action=='add_response')
+{
+
+}
+else if($action=='edit_college')
+{
+
+}
+else if($action=='delete_college')
+{
+
+}
+else if($action=='alumni_filter')
+{
+    include('../view/alumni_filter.php');
+}
+else
+{
+    include('index.php');
+}
 ?>
 <html>
 <head>
